bf6eb83d3ded5388cec879c85bb46dc2fb36d5a3
nancy-kitihoun <nancy.kitihoun@yahoo.com>
2019-11-12 23:16:37 +0100

    try

---
+++ server.R
@@ -0,0 +1,215 @@
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(reshape)
+library(funModeling)
+library(GGally)
+library(dplyr)
+library(datasets)
+library(ggplot2)
+library(e1071)
+library(randomForest)
+library(tree)
+library(caret)
+library(parallelSVM)
+library(gbm)
+
+options(shiny.maxRequestSize=150*1024^2)
+
+
+plot_num_density <- function (data, path_out = NA) 
+{
+  wide_data = suppressMessages(melt(data))
+  p = ggplot(data = wide_data,  aes(x = value)) + 
+    geom_density( na.rm = T) + 
+    facet_wrap(~variable, 
+               scales = "free") + aes(fill = variable) + guides(fill = FALSE)
+  if (!is.na(path_out)) {
+    export_plot(p, path_out, "density")
+  }
+  plot(p)
+}
+
+tabl=function(data,b){
+  
+  set.seed(123)
+  attach(data)
+  da1=data[Class==1,]
+
+  dao=data[Class==0,]
+  no=nrow(dao)
+  
+  p=which(Class==0)
+  
+  ind=sample(p, no*b)
+  part=data[ind,]
+  
+  new=rbind(part,da1)
+  neawdat=sample(1:nrow(new),nrow(new))
+  newdata=new[neawdat,]
+  attach(newdata)
+  newdata
+}
+
+# b pouvent aller de 0,001 à 0.005
+
+# ind=sample(2, nrow(data()), replace=TRUE ,prob=c(0.7, 0.3))
+# 
+# train=data[ind==1,]
+# test=data[ind==2,]
+# 
+# 
+# und=reactive({
+#   s=ovun.sample(Class~., data=train(), method="under", N=1000)
+#   s$data
+#   
+# })  
+
+shinyServer(function(input, output,session) {
+    
+    #importation 
+    
+    #saveRDS(data,file='C:/Users/camille/Desktop/Documents/GitHub/Application/genre/creditcard.rds')
+    
+ data=reactive({
+   
+ readRDS(file='D:\Github\Application\genre')
+   
+   
+   
+   })
+    set.seed(1234)
+  
+    output$dataTable = DT::renderDataTable( {
+              datatable(data(), extensions = 'FixedColumns',editable = 'cell',
+                        rownames = F, options = list(pageLength = 5, dom = 'tp',scrollX = TRUE,
+                                                     fixedColumns = TRUE),
+                                                                 selection = list(mode = "single",
+                                                                    target = "column", selected = 4)
+                             )
+    })
+    output$Des1 =DT::renderDataTable({ 
+                                    datatable(profiling_num(data()),extensions = 'FixedColumns',editable = 'cell',
+                                              rownames = F, options = list(pageLength = 5, dom = 'tp',scrollX = TRUE,
+                                                                           fixedColumns = TRUE),
+                                              selection = list(mode = "single",
+                                                               target = "column", selected = 4) )})
+    
+    output$Des2 =DT::renderDataTable({ 
+      datatable(df_status(data()),extensions = 'FixedColumns',editable = 'cell',
+                rownames = F, options = list(pageLength = 5, dom = 'tp',scrollX = TRUE,
+                                             fixedColumns = TRUE),
+                selection = list(mode = "single",
+                                 target = "column", selected = 4) )})
+    
+    m=reactive({
+      switch(input$variable1,
+                   
+                   "Time"= Time , "V1"=V1 ,"V2"=V2, "V3"=V3 ,"V4"=V4, "V5"=V5,"V6"=V6,"V7"=V7 ,"V8"=V8 ,
+                   "V9"=V9,"V10"=V10,"V11"=V11 , "V12"=V12,"V13"=V13,"V14"=V14  , 
+                   "V15"=V15, "V16"=V16   , "V17"=V17   , "V18"=V18 ,   "V19"=V19  ,  "V20"=V20  ,  "V21"=V21  ,  
+                   "V22"=V22, "V23"=V23, "V24"=V24, "V25"=V25 , "V26"=V26, "V27"=V27  ,  "V28"=V28 , "Amount"=Amount)
+      
+    
+      
+    })
+    
+    n=reactive({
+      switch(input$variable2,
+                   
+                   "Time"= Time , "V1"=V1 ,"V2"=V2, "V3"=V3 ,"V4"=V4, "V5"=V5,"V6"=V6,"V7"=V7 ,"V8"=V8 ,
+                   "V9"=V9,"V10"=V10,"V11"=V11 , "V12"=V12,"V13"=V13,"V14"=V14  , 
+                   "V15"=V15, "V16"=V16   , "V17"=V17   , "V18"=V18 ,   "V19"=V19  ,  "V20"=V20  ,  "V21"=V21  ,  
+                   "V22"=V22, "V23"=V23, "V24"=V24, "V25"=V25 , "V26"=V26, "V27"=V27  ,  "V28"=V28 , "Amount"=Amount)
+      
+      
+      
+    })
+    
+    
+    output$Grap1 <- renderPlot({ggplot(data(), aes(x=m(), y=n()))})
+    output$Grap2<- renderPlot({freq(data()[,31])})
+    output$tabCor <-renderPlot({ ggcorr(data()[,-31])})
+    
+    # pour une page qui affiche tout un graph complet 
+    
+  
+  
+    output$Grap3<- renderPlot({plot_num_density(data()[,c(-31)])})
+    
+    # partie analyse
+    # svm
+       train=reactive({
+                         new=tabl(data(),0.002)
+                       ind=sample(1:nrow(new),nrow(new)*0.7)
+                       tr=new[ind,]
+                       tr
+       })
+       
+       test=reactive({
+         new=tabl(data(),0.002)
+         ind=sample(1:nrow(new),nrow(new)*0.7)
+         tes=new[-ind,]
+         tes
+         
+       })
+      
+      svm=reactive({
+        
+      s=parallelSVM(Class~.,data=train(), kernel="linear",scale=F, type= 'C')
+      s
+            })
+       
+      output$svm=renderPrint(svm())
+      
+     
+    
+    predicte=reactive({
+      
+    svm.pred=predict(svm(),test()[,-31])
+    #svm.pred=as.factor(svm.pred1)
+    t=confusionMatrix(svm.pred,test())
+    t
+    })
+  
+    output$performence =renderPrint({predicte()})
+    
+    
+    #roc=reactive({Sys.sleep(1)
+        #roc.curve(predicte(),curve = TRUE,max.compute = TRUE, 
+                #  min.compute = TRUE, rand.compute = TRUE)})
+    
+   # output$ROC=reactive({renderPlot( {plot(roc(),max.plot = TRUE, 
+    #                                       min.plot = TRUE, rand.plot = TRUE, fill.area = TRUE)})
+     # })
+    
+    #output$ROC= renderPlot( {plot(roc(),max.plot = TRUE, min.plot = TRUE, rand.plot = TRUE, fill.area = TRUE)})
+    
+    p= roc.curve(met.pred,test$Class,curve = TRUE,max.compute = TRUE, 
+                 min.compute = TRUE, rand.compute = TRUE)
+    plot(p,max.plot = TRUE, min.plot = TRUE, rand.plot = TRUE, fill.area = TRUE)
+    
+    #abre de classification
+     tre=reactive({
+       tree(Class~., train())})
+     
+    output$tree=renderPrint({ summary(tre())})
+    
+    output$abre= renderPlot({plot(tre())
+                          text(tre(), pretty=0)
+      
+    })
+    
+    
+    
+    # gradient boosting
+    boost=reactive({
+      trai=as.data.frame(train())
+      g=gbm(class~., data=trai, distribution="gaussian", n.trees= 500, interaction=4)
+      g})
+    
+    output$gradient=renderPrint(summary(boost()))
+    
+    
+  })
+    
---
+++ ui.R
@@ -0,0 +1,178 @@
+library(shiny)
+library(shinydashboard)
+
+
+
+dashboardPage(title = "Détection des fraudes sur les cartes de crédits",
+              skin = ("green"),
+              dashboardHeader(title = "Détection des fraudes sur les cartes de crédits", titleWidth=800),
+              
+              dashboardSidebar(
+                sidebarMenu(
+                  menuItem("Introduction", tabName = "intro", icon = icon("home")),
+                  menuItem("Données", tabName = "datafile", icon = icon("table")),
+                  menuItem("Analyse", tabName = "analysis", icon = icon("chart-bar")
+                  ),
+                  menuItem("Paper", tabName = "paper", icon = icon("file-pdf-o")),
+                  hr(),
+                  sidebarUserPanel(name = a("Camille AZANGHO",
+                                            href = "https://www.univ-orleans.fr/deg/masters/ESA/etu/promo_M22020.html"),
+                                   subtitle = "brunellekas@gmail.com"),br(),
+                  sidebarUserPanel(name = a("MAKHOKH Lamyae",
+                                            href = "https://www.univ-orleans.fr/deg/masters/ESA/etu/promo_M22020.html"),
+                                   subtitle = "lamayae1993makhokh@gmail.com"),br(),
+                  sidebarUserPanel(name = a("KITIHOUN Nancy",
+                                            href = "https://www.univ-orleans.fr/deg/masters/ESA/etu/promo_M22020.html"),
+                                   subtitle = "nancy.kitihoun@yahoo.com")
+                  
+                  
+                  
+                  
+                ),
+                tags$head(tags$style(HTML(
+                  '/*header title font*/
+.main-header .logo{
+  font-family: "Geogia",Times,"Times New Roman",Serif;
+  font-weight:  bold;
+  font-size: 24px;
+}
+
+/*background color of hearder (logo part)*/
+.skin-blue  .main-header .logo{
+  background-color: #8a6777;
+}
+
+/* change the background color of hearder (logo part) when mouse hover */
+ .skin-blue .main-header .logo:hover {
+   background-color:orange;
+ }
+ 
+ /*background color for remaining part of the header */
+ .skin-blue .main-header .navbar {
+   background-color: #67818a;
+ }
+ 
+ /* main sidebar */
+   .skin-blue .main-sidebar {
+           background-color:#67818a;
+   }
+ 
+ 
+ /* active sidebar menu item */
+ 
+    .skin-blue .main-sidebar .sidebar-menu .active a{
+      background-color: white;
+      color:black;
+      
+    }
+    /*  sidebar menuitems */
+        .skin-blue .main-sidebar .sider .sidebar-menu a{
+                   background-color: orange;
+                          color:white;
+      
+        }
+    /* sider menuitems when mouse hover */
+    
+     .skin-blue .main-sidebar  .sidebar .sidebar-menu a:hover{
+        background-color: brown;
+        color:white;
+        
+     }
+     /* sidebar toggle button */
+     .skin-blue .main-header  .navbar .sidebar-toggle:hover {
+       background-color:black;
+     }
+        
+                              ')))
+                
+              ),
+              dashboardBody(
+                
+                
+                tabItems(
+                  # Read data
+                  tabItem(tabName = "intro", includeHTML("intro.html")),    
+                  
+                  
+                  
+                  tabItem(tabName = "datafile",
+                          style = "overflow-y:scroll;",
+                          
+                          # The id lets us use input$tabset1 on the server to find the current tab
+                          height = "250px",
+                          tabBox(selected = "Description1",width = 12,
+                                 tabPanel("Description1",
+                                          box(width = 12, status = "primary",
+                                              title = "detection de fraude sur des cartes bancaires", 
+                                              DT::dataTableOutput("dataTable")
+                                          ),
+                                          box(width = 6,status = "warning", 
+                                              title=" simple statistique descriptive",
+                                              DT::dataTableOutput("Des1")),
+                                          box(width = 6,status = "info",
+                                              "le nombre de valeurs nulles (q_zeros) et son pourcentage (p_zeros)",br(),
+                                              "le nombre de valeurs manquantes (q_na) et son pourcentage (p_na)",br(),
+                                              "le nombre de valeur infinies (q_inf) et son pourcentage (p_inf)",br(),
+                                              
+                                              DT::dataTableOutput("Des2"))),
+                                 tabPanel("Description2",
+                                          
+                                          
+                                          box(with=12 , status = "warning",
+                                              
+                                              sidebarLayout(
+                                                sidebarPanel(
+                                                  #Definition 1er menu deroulant
+                                                  selectInput("variable1", "Variable (axe des x):",
+                                                              list("Time" , "V1" ,"V2", "V3" ,"V4" , "V5" ,"V6","V7" ,"V8" ,
+                                                                   "V9","V10","V11" , "V12","V13","V14"  , 
+                                                                   "V15", "V16"   , "V17"   , "V18" ,   "V19"  ,  "V20"  ,  "V21"  ,  
+                                                                   "V22", "V23", "V24", "V25" , "V26", "V27"  ,  "V28" , "Amount" )),
+                                                  
+                                                  #Definition du second menu deroulant
+                                                  selectInput("variable2", "Variable (axe des y):",
+                                                              list( "V1" ,"V2", "V3" ,"V4" , "V5" ,"V6","V7" ,"V8" ,
+                                                                    "V9","V10","V11" , "V12","V13","V14"  , 
+                                                                    "V15", "V16"   , "V17"   , "V18" ,   "V19"  ,  "V20"  ,  "V21"  ,  
+                                                                    "V22", "V23", "V24", "V25" , "V26", "V27"  ,  "V28" , "Amount","Time"))),
+                                                
+                                                mainPanel(  plotOutput("Grap1", height = "300px")))),
+                                          box( status = "info",  plotOutput("Grap2", height = "300px")),
+                                          box( title="CorrÃ©lation", plotOutput("tabCor") ,status = "primary")
+                                 ),
+                                 tabPanel("Un Tout",status = "info",width = 24,
+                                          plotOutput("Grap3", width = "100%" ))
+                                 
+                          )),
+                  # Analyse
+                  tabItem(tabName="analysis",
+                          style = "overflow-y:scroll;",
+                          sliderInput("proportion ", "proportion 0", min=0.001, max=0.005, value=0.003,
+                                      width = '95%'),
+                          
+                          tabBox(type = "pills",selected = "SVM",width = 12,
+                                 
+                                 tabPanel("SVM", 
+                                          
+                                          box(width = 6, verbatimTextOutput("svm"),status = "primary", title="sortie svm"),
+                                          box(width=6,verbatimTextOutput("performence"),status = "primary",title="matrice de confusion")
+                                          #box(plotOutput("ROC", height = "300px" ),status = "primary",title="courbe AUC")
+                                          
+                                          
+                                 ),
+                                 tabPanel("Abre de classification",
+                                          box(width = 6, verbatimTextOutput("tree"),status = "primary", title="sortie tree"),
+                                          box(width = 6, plotOutput("abre", height = "300px" ),status = "primary"),
+                                          box(width = 6, plotOutput("tre_roc", height = "300px" ),status = "primary")
+                                          
+                                 ),
+                                 tabPanel("gradient boosting",
+                                          box(width = 6, plotOutput("gradient"),status = "primary", title="gradient boosting"),
+                                          box(width = 6, verbatimTextOutput("perfgradient" ),status = "primary")
+                                          
+                                 )))
+                  
+                  
+                  
+                  
+                )))
